0aafb5f139d4a134b394f2a08db85eaa0328bd97
NathanPerez18 <45292441+NathanPerez18@users.noreply.github.com>
2024-11-18 20:21:45 -0500

    commented out

--- lib/NewNote.dart
+++ lib/NewNote.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:path_provider/path_provider.dart';
 import 'note.dart';
-import 'database_handler.dart';
+import 'DatabaseHandler.dart';
 
 class NewNotePage extends StatefulWidget {
   final Note? editableNote;
@@ -69,73 +69,73 @@ class _NewNotePageState extends State<NewNotePage> {
     Navigator.pop(context);
   }
 
-  Future<void> _pickImage() async {
-    final ImagePicker picker = ImagePicker();
-    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+  // Future<void> _pickImage() async {
+  //   final ImagePicker picker = ImagePicker();
+  //   final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+  //
+  //   if (image != null) {
+  //     String filePath = await _saveImageToInternalStorage(image);
+  //     setState(() {
+  //       _photoPath = filePath;
+  //     });
+  //   }
+  // }
 
-    if (image != null) {
-      String filePath = await _saveImageToInternalStorage(image);
-      setState(() {
-        _photoPath = filePath;
-      });
-    }
-  }
+  // Future<void> _takePhoto() async {
+  //   final ImagePicker picker = ImagePicker();
+  //   final XFile? image = await picker.pickImage(source: ImageSource.camera);
+  //
+  //   if (image != null) {
+  //     String filePath = await _saveImageToInternalStorage(image);
+  //     setState(() {
+  //       _photoPath = filePath;
+  //     });
+  //   }
+  // }
 
-  Future<void> _takePhoto() async {
-    final ImagePicker picker = ImagePicker();
-    final XFile? image = await picker.pickImage(source: ImageSource.camera);
+  // Future<String> _saveImageToInternalStorage(XFile image) async {
+  //   Directory appDir = await getApplicationDocumentsDirectory();
+  //   String fileName = "image_${DateTime.now().millisecondsSinceEpoch}.jpg";
+  //   String filePath = "${appDir.path}/$fileName";
+  //
+  //   final File newImage = await File(image.path).copy(filePath);
+  //   return newImage.path;
+  // }
 
-    if (image != null) {
-      String filePath = await _saveImageToInternalStorage(image);
-      setState(() {
-        _photoPath = filePath;
-      });
-    }
-  }
+  // Future<void> _deleteImage() async {
+  //   if (_photoPath != null) {
+  //     final File file = File(_photoPath!);
+  //     if (await file.exists()) {
+  //       await file.delete();
+  //     }
+  //     setState(() {
+  //       _photoPath = null;
+  //     });
+  //   }
+  // }
 
-  Future<String> _saveImageToInternalStorage(XFile image) async {
-    Directory appDir = await getApplicationDocumentsDirectory();
-    String fileName = "image_${DateTime.now().millisecondsSinceEpoch}.jpg";
-    String filePath = "${appDir.path}/$fileName";
-
-    final File newImage = await File(image.path).copy(filePath);
-    return newImage.path;
-  }
-
-  Future<void> _deleteImage() async {
-    if (_photoPath != null) {
-      final File file = File(_photoPath!);
-      if (await file.exists()) {
-        await file.delete();
-      }
-      setState(() {
-        _photoPath = null;
-      });
-    }
-  }
-
-  void _selectColor() async {
-    Color? pickedColor = await showDialog<Color>(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text("Select Note Color"),
-        content: SingleChildScrollView(
-          child: BlockPicker(
-            pickerColor: Color(_selectedColor),
-            onColorChanged: (color) {
-              Navigator.pop(context, color);
-            },
-          ),
-        ),
-      ),
-    );
-
-    if (pickedColor != null) {
-      setState(() {
-        _selectedColor = pickedColor.value;
-      });
-    }
-  }
+  // void _selectColor() async {
+  //   Color? pickedColor = await showDialog<Color>(
+  //     context: context,
+  //     builder: (context) => AlertDialog(
+  //       title: Text("Select Note Color"),
+  //       content: SingleChildScrollView(
+  //         child: BlockPicker(
+  //           pickerColor: Color(_selectedColor),
+  //           onColorChanged: (color) {
+  //             Navigator.pop(context, color);
+  //           },
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  //
+  //   if (pickedColor != null) {
+  //     setState(() {
+  //       _selectedColor = pickedColor.value;
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -179,34 +179,34 @@ class _NewNotePageState extends State<NewNotePage> {
               ),
               maxLines: 5,
             ),
-            SizedBox(height: 10),
-            if (_photoPath != null)
-              Column(
-                children: [
-                  Image.file(File(_photoPath!)),
-                  TextButton(
-                    onPressed: _deleteImage,
-                    child: Text("Remove Image"),
-                  ),
-                ],
-              ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.image),
-                  onPressed: _pickImage,
-                ),
-                IconButton(
-                  icon: Icon(Icons.camera),
-                  onPressed: _takePhoto,
-                ),
-                IconButton(
-                  icon: Icon(Icons.color_lens),
-                  onPressed: _selectColor,
-                ),
-              ],
-            ),
+            // SizedBox(height: 10),
+            // if (_photoPath != null)
+            //   Column(
+            //     children: [
+            //       Image.file(File(_photoPath!)),
+            //       TextButton(
+            //         onPressed: _deleteImage,
+            //         child: Text("Remove Image"),
+            //       ),
+            //     ],
+            //   ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //   children: [
+            //     IconButton(
+            //       icon: Icon(Icons.image),
+            //       onPressed: _pickImage,
+            //     ),
+            //     IconButton(
+            //       icon: Icon(Icons.camera),
+            //       onPressed: _takePhoto,
+            //     ),
+            //     IconButton(
+            //       icon: Icon(Icons.color_lens),
+            //       onPressed: _selectColor,
+            //     ),
+            //   ],
+            // ),
           ],
         ),
       ),
